195819a882324f5d35196ac46d214ab921c4f480
Aaron Anderson <aaronlynanderson@gmail.com>
2020-01-24 19:25:19 -0600

    :fire: remove code

--- ching-plugin/src/ChingPlugin.ts
+++ ching-plugin/src/ChingPlugin.ts
@@ -1,27 +1,10 @@
 import { BurnerPluginContext, Plugin, Actions } from '@burner-wallet/types';
-
-interface PluginActionContext {
-  actions: Actions;
-}
+import axios from "axios";
 
 export default class ChingPlugin implements Plugin {
   private pluginContext?: BurnerPluginContext;
 
   initializePlugin(pluginContext: BurnerPluginContext) {
     this.pluginContext = pluginContext;
-
-    onQRScanned: ((scan: string, ctx: PluginActionContext) => {
-      if (scan === 'My Plugin') {
-        ctx.actions.navigateTo('/my-page');
-        return true;
-      }
-    });
-  }
-
-  async getBlockNum() {
-    const assets = this.pluginContext!.getAssets();
-    const web3 = this.pluginContext!.getWeb3(assets[0].network);
-    const blockNum = web3.eth.getBlockNumber();
-    return blockNum;
   }
 }
