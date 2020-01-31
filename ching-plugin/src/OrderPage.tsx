ebe4cbd026a19332ba51b006220316496fcf8f56
Aaron Anderson <aaronlynanderson@gmail.com>
2020-01-31 13:15:59 -0600

    solving corb

--- ching-plugin/src/OrderPage.tsx
+++ ching-plugin/src/OrderPage.tsx
@@ -36,7 +36,9 @@ const OrderPage: React.FC<PluginPageContext> = ({ location, BurnerComponents, ac
 
   axios.get("https://us-central1-daipos.cloudfunctions.net/itemDetails?itemId=RHfVDGM5L2BKPaIwGOXA", {
     headers: {
+      'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
+      'crossdomain': true
     }
   })
     .then(response => {
