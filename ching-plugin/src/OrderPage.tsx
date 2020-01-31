19ae2f5a8b7dcd51ad93eaa6ef7c21fbc46331e3
Aaron Anderson <aaronlynanderson@gmail.com>
2020-01-31 14:33:49 -0600

    tinkering

--- ching-plugin/src/OrderPage.tsx
+++ ching-plugin/src/OrderPage.tsx
@@ -17,16 +17,6 @@ const OrderPage: React.FC<PluginPageContext> = ({ location, BurnerComponents, ac
     );
   }
 
-  const continueCheckout = () => {
-    actions.send({
-      to: txDetails.to,
-      asset: txDetails.tokenName.toLowerCase(),
-      ether: txDetails.amount,
-      id: txDetails.orderId,
-      message: `Ching order - "${note}"`
-    });
-  };
-
   // just need to render some data from the endpoint
   // let itemizedList = () => {
   //   axios.get("https://us-central1-daipos.cloudfunctions.net/orderDetails?orderId=13VwSGhmVuwjpLKFmqd7")
@@ -35,17 +25,27 @@ const OrderPage: React.FC<PluginPageContext> = ({ location, BurnerComponents, ac
   let itemizedList;
 
   axios.get("https://us-central1-daipos.cloudfunctions.net/itemDetails?itemId=RHfVDGM5L2BKPaIwGOXA"
-    // , {
-    //   headers: {
-    //     'Content-Type': 'document',
-    //     'Access-Control-Allow-Origin': '*',
-    //     'crossdomain': true
-    //   }
-    // }
+    , {
+      headers: {
+        'Content-Type': 'text-plain',
+        'Access-Control-Allow-Origin': '*',
+        'crossdomain': true
+      }
+    }
   )
-    .then(response => {
-          itemizedList = response;
-        });
+  .then(response => {
+    itemizedList = response;
+  });
+
+  const continueCheckout = () => {
+    actions.send({
+      to: txDetails.to,
+      asset: txDetails.tokenName.toLowerCase(),
+      ether: txDetails.amount,
+      id: txDetails.orderId,
+      message: `You shared "${note}" with the message board and you ordered: ${itemizedList}`
+    });
+  };
 
   return (
     <Page title="Ching Checkout">
