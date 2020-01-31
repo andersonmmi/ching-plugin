4f4c1f66b004d83e2692f1a0a242927a22ea90cd
Aaron Anderson <aaronlynanderson@gmail.com>
2020-01-31 13:22:10 -0600

    add ERC20Asset: 'it'

--- basic-wallet/src/index.tsx
+++ basic-wallet/src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { xdai, dai, eth } from '@burner-wallet/assets';
+import { xdai, dai, eth, ERC20Asset } from '@burner-wallet/assets';
 import BurnerCore from '@burner-wallet/core';
 import { InjectedSigner, LocalSigner } from '@burner-wallet/core/signers';
 import { InfuraGateway, InjectedGateway, XDaiGateway, } from '@burner-wallet/core/gateways';
 import Exchange, { Uniswap, XDaiBridge } from '@burner-wallet/exchange';
 import ModernUI from '@burner-wallet/modern-ui';
 import ChingPlugin from 'ching-plugin';
+
+const it = new ERC20Asset({
+  id: "it",
+  name: "IT",
+  network: "100",
+  address: "0xd203308fff6ba932d7cdb8bbe12aea9d57204205"
+});
 
 const core = new BurnerCore({
   signers: [new InjectedSigner(), new LocalSigner()],
@@ -15,7 +22,7 @@ const core = new BurnerCore({
     new InfuraGateway(process.env.REACT_APP_INFURA_KEY),
     new XDaiGateway(),
   ],
-  assets: [xdai, dai, eth],
+  assets: [xdai, it, dai, eth],
 });
 
 const exchange = new Exchange({
