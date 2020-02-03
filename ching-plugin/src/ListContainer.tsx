dd95382c32b086b6af0152b1c601eee607e2cb80
Aaron Anderson <aaronlynanderson@gmail.com>
2020-02-03 02:48:27 -0600

    walking back changes

---
+++ ching-plugin/src/ListContainer.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { PluginPageContext } from '@burner-wallet/types';
+import { getTxDetails } from './ChingPlugin';
+import ItemsList from "./ItemsList";
+
+const CheckoutItemsList: React.FC<PluginElementContext & {tx: any}> = ({ tx }) => {
+  return (
+    <ItemsList orderId={tx.id} />
+  );
+}
