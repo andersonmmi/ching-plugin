c42ec54b9087c5e79a50326bd052ad4025e1762d
David Mihal <david@dmihal.com>
2020-02-03 09:39:40 -0500

    Hide checkout item list when no ID provided

--- ching-plugin/src/CheckoutItemsList.tsx
+++ ching-plugin/src/CheckoutItemsList.tsx
@@ -4,7 +4,10 @@ import { PluginElementContext } from '@burner-wallet/types';
 import ItemsList from "./ItemsList";
 
 const CheckoutItemsList: React.FC<PluginElementContext & {tx: any}> = ({ tx }) => {
-  console.log("tx", tx)
+  if (!tx.id) {
+    return null;
+  }
+
   return (
     <ItemsList orderId={tx.id} />
   );
